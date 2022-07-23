2589e0aca5b7a121a57496bc238b2587d5ea75e7
Paulina <pau.jastrzab@gmal.com>
2022-07-23 12:49:51 +0200

    added loop

---
+++ src/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main (String[] args) {
+        int x = 3;
+        int y = 3;
+        System.out.println(x++);
+        System.out.println(++y);
+
+    }
+
+}
