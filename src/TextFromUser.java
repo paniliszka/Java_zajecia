c1f79be6bfee27489a90b6b1fa30a212099d3bf7
Paulina <pau.jastrzab@gmal.com>
2022-07-23 11:53:50 +0200

    added question

--- src/TextFromUser.java
+++ src/TextFromUser.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class TextFromUser {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Who are you?");
         String userInput = scanner.nextLine();
-        System.out.println("Hi there"+userInput);
+        System.out.println("Hi there, " +userInput);
 
     }
 }
